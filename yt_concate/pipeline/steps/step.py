fb568e601d3aba691fb5ec2c4fe316c60bf7ffda
Corring <ningyi@asu.edu>
2021-09-01 16:26:48 -0700

    Update step.py

--- yt_concate/pipeline/steps/step.py
+++ yt_concate/pipeline/steps/step.py
@@ -6,15 +6,10 @@ class Step(ABC):
     def __init__(self):
         pass
 
-    # with @abstructmethod
-    # inhereient must have this method
-    # input 在这里做字典的用处
     @abstractmethod
     def process(self, data, inputs):
         pass
 
 
-# 捕捉except的method
-# Exception is buit-in
 class StepException(Exception):
     pass
