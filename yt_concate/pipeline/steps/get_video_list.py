cb7a599699b397ddb841fc46339607a9152c59f4
Corring <ningyi@asu.edu>
2021-09-01 16:26:32 -0700

    Update get_video_list.py

--- yt_concate/pipeline/steps/get_video_list.py
+++ yt_concate/pipeline/steps/get_video_list.py
@@ -9,9 +9,8 @@ class GetVideoList(Step):
     def process(self, data, inputs):
         channel_id = inputs['channel_id']
 
-        # v= 后头有的时候会接ID，通常是漫长一段字串。这里是基底
+       
         base_video_url = 'https://www.youtube.com/watch?v='
-        # API的网址，
         base_search_url = 'https://www.googleapis.com/youtube/v3/search?'
 
         first_url = base_search_url + 'key={}&channelId={}&part=snippet,id&order=date&maxResults=25'.format(
@@ -22,13 +21,9 @@ class GetVideoList(Step):
         url = first_url
 
         while True:
-            # 在这个part很容易出错，因为API的限制使用次数
-            # 链接网址
             inp = urllib.request.urlopen(url)
-            # 回传结果
             resp = json.load(inp)
 
-            # 做结果的解读
             for i in resp['items']:
                 if i['id']['kind'] == "youtube#video":
                     video_links.append(base_video_url + i['id']['videoId'])
@@ -42,5 +37,4 @@ class GetVideoList(Step):
 
         print(len(video_links))
 
-        # return 到pipeline的data中，后传递到下一步
         return video_links
